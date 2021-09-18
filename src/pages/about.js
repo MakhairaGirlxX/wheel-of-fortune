7092ee4f4e1094908a967ccb5d470d1c8a3caae1
MakhairaGirlxX <gracet.forciea@gmail.com>
2021-09-18 14:10:01 -0500

    Constructed Basic Website Setup

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../buildcomponents/layout'
+
+const AboutPage = () => {
+    return(
+      <Layout pageTitle="About Game">
+          <p>About Wheel of Fortune Game</p>
+      </Layout>
+    )
+}
+
+export default AboutPage
