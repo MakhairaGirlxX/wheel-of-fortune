9c2f7d8f8a6a53b315e86c920725c628461810a4
Adam Nuxoll <adamnux@gmail.com>
2021-09-22 19:34:06 -0500

    Testing creating page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../buildcomponents/layout'
+
+const HomePage = () => {
+    return(
+      <Layout pageTitle="Home Page!">
+          <p>Home page of Wheel of Fortune Game</p>
+      </Layout>
+    )
+}
+
+export default HomePage
