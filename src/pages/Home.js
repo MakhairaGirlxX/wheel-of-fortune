f7c1322ce3fce2f2e8c1be3978ff6c6a33bae838
Dalton <dhein@carrollu.edu>
2021-11-03 19:16:20 -0500

    SpinButton Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import * as React from 'react'
 import Layout from '../buildcomponents/layout'
 import PlayerList from '../Components/players/playerlist.component'
-
+import SpinButton from '../Components/spinButton.component'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import { Container, Row, Col, Image, Button } from 'react-bootstrap'
-import { useEffect, useState } from "react";
 //import { render } from 'react-dom'      *Currently unused, keep for later if needed*
 
 // markup
@@ -16,21 +15,6 @@ const HomePage = () => {
   {playername: "player2name", funds: 600, id: "2b"},
   {playername: "player3name", funds: 400, id: "3c"},
   {playername: "player4name", funds: 200, id: "4d"}];
-
-  // Button animation
-  const [isSpinning, setSpinning] = useState(false);
-  const handleClick = () => setSpinning(true);
-  useEffect(() => {
-    if (isSpinning) {
-      simulateNetworkRequest().then(() => {
-        setSpinning(false);
-      });
-    }
-  }, [isSpinning]);
-
-  function simulateNetworkRequest() {
-    return new Promise((resolve) => setTimeout(resolve, 2000));
-  }
   
   
   return (
@@ -38,14 +22,7 @@ const HomePage = () => {
       <Container fluid>
         <Row>
           <Col sm={{span:3, offset:6}}>
-              <Image src="WheelofFortune.png" thumbnail /><br />
-              <Button
-                variant="primary"
-                disabled={isSpinning}
-                onClick={!isSpinning ? handleClick : null}
-              >
-                {isSpinning ? 'Spinning...' : 'Spin'}
-              </Button>
+              <SpinButton isVisible={true} />
           </Col>
           <Col sm={{span:3}}>
               <PlayerList playerlist={plist}/>
