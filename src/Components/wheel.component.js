f5f3ee68765564a7c950940dac6b138ddfda6423
Dalton <dhein@carrollu.edu>
2021-11-11 18:47:55 -0600

    CSS animation for spinning wheel

--- src/Components/wheel.component.js
+++ src/Components/wheel.component.js
@@ -1,74 +1,24 @@
 import React from 'react';
 import { Container, Button } from 'react-bootstrap';
+import '../styles/wheel.css'
 
-class Wheel extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { rotation: 0, spin: 0 }
-        this.rotate = this.rotate.bind(this);
+class Wheel extends React.Component {
+    constructor() {
+        super();
+        this.state = {isSpinning: false}
         this.spin = this.spin.bind(this);
     }
-    
-    rotate() {
-        let newRotation = this.state.rotation + 45;
-        if (newRotation >= 360) { newRotation =- 360; }
-        this.setState({ rotation: newRotation, })
-    }
-    
+
     spin() {
-        let newSpin = this.state.spin + 45;
-        if (newSpin >= 360) { newSpin =- 360; }
-        this.setState({ spin: newSpin, })
-
-        /*
-        function delay(time) {
-            return new Promise((resolve) => setTimeout(resolve, time));
-        }
-
-        for (let i=0; i<1; i++) {
-            delay(500).then(() => {
-                newSpin += 45;
-                if (newSpin >= 360) { newSpin =- 360; }
-                this.setState({ spin: newSpin, })
-                
-                delay(500).then(() => {
-                    newSpin += 45;
-                    if (newSpin >= 360) { newSpin =- 360; }
-                    this.setState({ spin: newSpin, })
-                });
-            });
-        }
-        */
-        
-        let spinCounter = 0;
-        function spinLoop() {
-            setTimeout(function() {
-                newSpin += 45;
-                if (newSpin >= 360) { newSpin =- 360; }
-                this.setState({ spin: newSpin, })
-
-                spinCounter++;
-                if (spinCounter<4) spinLoop();
-            }, 500)
-        }   
-        spinLoop();
+        this.setState({isSpinning: !this.state.isSpinning})
     }
 
     render() {
-        const { rotation, spin } = this.state;
+        const { isSpinning } =  this.state;
+
         return <Container>
-            <img style={{
-                transform: `rotate(${rotation}deg)`}}
-                src={"https://wheel.fhtl.byu.edu/images/wheel.png"}
-                width="300"
-                height="300"
-            />
+            <div className={`wheel ${isSpinning ? 'spin' : ''}`} />
             <br /><br /><br />
-            <Button
-                variant="primary"
-                onClick={this.rotate}
-                >Rotate
-            </Button>
             <Button
                 variant="primary"
                 onClick={this.spin}
@@ -76,9 +26,5 @@ class Wheel extends React.Component{
             </Button>
         </Container>
     }
-  }
-  
-  const element = <Wheel /> 
-  
+}
 export default Wheel;
-  
