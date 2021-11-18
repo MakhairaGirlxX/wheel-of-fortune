dfe4267735df9142107c229649718ece89373ffa
Jkenyon <jkenyon@carrollu.edu>
2021-11-17 18:42:18 -0600

    Moved Files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Layout from '../buildcomponents/layout'
 import PlayerList from '../Components/players/playerlist.component'
 import SpinButton from '../Components/spinButton.component'
-import LetterBankFactory from './letterbank.js'
 
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -19,12 +18,7 @@ const HomePage = () => {
     <Layout pageTitle="Wheel Of Fortune!">
       <Game/>
     </Layout>
-  const consonantArr = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
-  const vowelArr = ["A", "E", "I", "O", "U", "Y"]
-  const plist = [{playername: "player1name", funds: 800, id: "1a"},
-     <Col> 
-        <LetterBankFactory consonantArr = {consonantArr} vowelArr = {vowelArr} vowelBool = {true}/>
-     </Col>
+  
   )
 }
 
