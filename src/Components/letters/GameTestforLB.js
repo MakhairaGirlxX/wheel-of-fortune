9468ecb0e3448938b5aec1f9e2e9dbdc7c7011b0
Jkenyon <jkenyon@carrollu.edu>
2021-12-08 18:16:33 -0600

    made test page for letterbank modal

---
+++ src/Components/letters/GameTestforLB.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ActionButtons from "../Components/actions/actionList.component";
+import LBModal from "../Components/letters/LttrBnkModal.js";
+
+
+class Game extends Component{
+
+}
